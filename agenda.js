082a95cbf612fdc6b1d397659750cca087adc35f
decocampos <70168225+decocampos@users.noreply.github.com>
2023-04-17 11:12:15 -0300

    Update agenda.js

--- agenda.js
+++ agenda.js
@@ -1,15 +1,12 @@
-function teste(){
-  var ui = SpreadsheetApp.getUi();
-  var response = ui.alert('Sala Indisponível no horário desejado', ui.ButtonSet.OK);
-}
-//Função para colocar a Data no formato correto
+// Normalize the format of date
 function formatDate(dateString) {
   var date = new Date(dateString);
   var options = {year: 'numeric', month: 'numeric', day: 'numeric'};
   var formattedDate = date.toLocaleDateString('en-US', options);
   return formattedDate;
 }
-//Função para colocar a Hora no formato correto
+
+// Normalize the format of time
 function formatTime(timeString) {
   var time = new Date(timeString);
   var options = {hour: 'numeric', minute: 'numeric', hour12: false};
@@ -17,45 +14,53 @@ function formatTime(timeString) {
   return formattedTime;
 }
 
-//Cria o evento no calendário
-  function createCalendarEvent() {
+//main function that create everything
+  function main() {
+    
     var sheet = SpreadsheetApp.getActiveSheet();
     var dataRange = sheet.getDataRange();
     var data = dataRange.getValues();
     var calendar = CalendarApp.getCalendarById('your@email.com');
     
-      //Pega a última linha
-      var lastRow = data.length - 1;
-      var row = data[lastRow];
-      var email = row[1];
-      var date = formatDate(row[2]);
-      var startTime = formatTime(row[3]);
-      var endTime = formatTime(row[4]);
-      var local = row[6];
-
-      //Pega os emails dos alunos para colocar como guests
-    var guestList = row[5].trim();
+//find last row == last response
+    var lastRow = data.length - 1;
+    var row = data[lastRow];
+    var email = row[2];
+    var date = formatDate(row[3]);
+    var startTime = formatTime(row[4]);
+    var endTime = formatTime(row[5]);
+    var local = row[6];
   
-      var eventTitle = '[Lingo Station] ' + local;
-      var eventDescription = 'INSIRA_A_DESCRIÇÃO_DO_EVENTO_AQUI';
-      
-      var start = new Date(date + ' ' + startTime);
-      var end = new Date(date + ' ' + endTime);
-      
-     // Check if calendar is free at the selected time
+//define the title and the description of the event
+    var eventTitle = '[Reserve] ' + local;
+    var eventDescription = 'Your reserve is done, the key is in the reception';
+         
+//Check if calendar is free at the selected time and also if the place is avaliable 
+  var start = new Date(date + ' ' + startTime);
+  var end = new Date(date + ' ' + endTime);
   var events = calendar.getEvents(start, end);
   var overlappingEvents = events.filter(function(event) {
     return event.getLocation() === local;
   });
+
+
   if (overlappingEvents.length > 0) {
-    // Display error message if there are overlapping events
-    var errorMessage = "You are not available at the selected time. Please select a different date/time.";
-    Logger.log(errorMessage);
+    // Send a email message if the place isn't avaliable
+    MailApp.sendEmail({
+      to: email,
+      subject: "unavailable",
+      htmlBody: "At this time this place it's already booked"
+    });
   } else {
-    // Create the new event
-    var event = calendar.createEvent(eventTitle, start, end, {description: eventDescription, guests: email+','+guestList, location: local});
+    // Create a event and Send a email message if the place is avaliable
+    var event = calendar.createEvent(eventTitle, start, end, {description: eventDescription, guests: email, location: local});
     event.addEmailReminder(60);
     event.addPopupReminder(30);
-    Logger.log(successMessage);
+       MailApp.sendEmail({
+      to: email,
+      subject: "Reserve Confirmed",
+      htmlBody: "It's confirmed"
+    });
   }
-    }
+}
+    
